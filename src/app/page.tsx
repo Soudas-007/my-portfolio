62a86b7307247cb9aa8bc4370ab1f03a1c198537
Soudas-007 <soudas.sur@gmail.com>
2026-05-11 11:45:36 +0530

    Update: page.tsx - use FooterSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 import HeroSection from "@/components/HeroSection";
-// import ScrollytellingCanvas from "@/components/ScrollytellingCanvas";
 import CaseStudies from "@/components/CaseStudies";
 import ExperienceSection from "@/components/ExperienceSection";
 import AboutSection from "@/components/AboutSection";
 import ToolboxSection from "@/components/ToolboxSection";
-import ContactSection from "@/components/ContactSection";
+import FooterSection from "@/components/FooterSection";
 import ScrollReveal from "@/components/ScrollReveal";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-background relative selection:bg-[var(--color-accent-blue)] selection:text-primary">
-      {/* Hero doesn't need ScrollReveal as it has its own entrance animations */}
       <HeroSection />
-
-      {/* <ScrollytellingCanvas /> */}
 
       <ScrollReveal>
         <CaseStudies />
@@ -31,9 +27,7 @@ export default function Home() {
         <ToolboxSection />
       </ScrollReveal>
 
-      <ScrollReveal>
-        <ContactSection />
-      </ScrollReveal>
+      <FooterSection />
     </main>
   );
 }
